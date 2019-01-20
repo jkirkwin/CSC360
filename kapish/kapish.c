3fc9c12e46e39a3043b4a198d03416dea787fb48
Jamie Kirkwin <jkirkwin@gmail.com>
2019-01-20 13:12:28 -0800

    Add initial files for kapish

---
+++ kapish/kapish.c
@@ -0,0 +1,30 @@
+/*
+ * Jamie Kirkwin
+ * jkirkwin 
+ * V00875987
+ * CSC360 Assignment 1, Kapish Shell
+ * Modified Jan 20, 2019
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // Load config file (.kapish)
+
+    // Loop until done
+
+    // Do teardown/termination
+
+    printf("Ye");
+
+    return 0;
+}
+
+int main_loop() {
+
+    // print marker (? )
+
+    // get input, tokenize, analyze, execute
+
+}
