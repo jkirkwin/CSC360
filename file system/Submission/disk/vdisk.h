931d7fe5f27a662006671b8c9d85f259ef169a15
jamie kirkwin <jkirkwin@gmail.com>
2019-04-11 12:37:04 -0700

    remove submission (confirmed on ssh)

--- file system/Submission/disk/vdisk.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-#define BYTES_PER_BLOCK 512
-#define BLOCKS_ON_DISK 4096 
-
-bool vdisk_read(int block_number, void *buffer, FILE *alt_disk);
-bool vdisk_write(int block_number, void *content, int offset, int content_length, FILE *alt_disk);
